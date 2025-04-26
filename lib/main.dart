67662835330a57afbbfb60f32ee48bafe7bba8e9
ilbagatto <sergei.krushinski@gmail.com>
2025-04-27 00:28:59 +0300

    common workarea

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class TarotApp extends StatelessWidget {
       title: 'Tarot App',
       theme: appTheme,
       onGenerateRoute: AppRouter.generateRoute,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
