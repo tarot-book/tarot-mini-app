de3625ed1899039b3af2fa47f3c2a66559170c61
ilbagatto <sergei.krushinski@gmail.com>
2025-04-29 16:01:04 +0300

    adopted to the proxy server

--- lib/services/api_client.dart
+++ lib/services/api_client.dart
@@ -1,16 +1,12 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:tarot_mini_app/constants/app_constants.dart';
 
 class ApiClient {
   final String _baseUrl;
-  final String _username;
-  final String _password;
 
-  ApiClient()
-      : _baseUrl = dotenv.env['API_BASE_URL'] ?? '',
-        _username = dotenv.env['API_USERNAME'] ?? '',
-        _password = dotenv.env['API_PASSWORD'] ?? '';
+  ApiClient() : _baseUrl = dotenv.env['API_BASE_URL'] ?? '';
 
   Future<List<dynamic>> getList(String endpoint) async {
     final url = Uri.parse('$_baseUrl$endpoint');
@@ -20,20 +16,18 @@ class ApiClient {
     return jsonDecode(response.body);
   }
 
-Future<Map<String, dynamic>> getObject(String endpoint) async {
-  final url = Uri.parse('$_baseUrl$endpoint');
-  final headers = _buildHeaders();
-  final response = await http.get(url, headers: headers);
-  _handleErrors(response);
-  return jsonDecode(response.body) as Map<String, dynamic>;
-}
-
+  Future<Map<String, dynamic>> getObject(String endpoint) async {
+    final url = Uri.parse('$_baseUrl$endpoint');
+    final headers = _buildHeaders();
+    final response = await http.get(url, headers: headers);
+    _handleErrors(response);
+    return jsonDecode(response.body) as Map<String, dynamic>;
+  }
 
   Map<String, String> _buildHeaders() {
-    final auth = 'Basic ${base64Encode(utf8.encode('$_username:$_password'))}';
     return {
-      'Authorization': auth,
       'Accept': 'application/json',
+      'User-Agent': 'TarotMiniApp/${AppConstants.appVersion}',
     };
   }
 
@@ -42,5 +36,4 @@ Future<Map<String, dynamic>> getObject(String endpoint) async {
       throw Exception('API error: ${response.statusCode} ${response.body}');
     }
   }
-
 }
