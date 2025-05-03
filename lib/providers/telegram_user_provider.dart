d4c9bdaddc1068ad79a5f690599bdb47384cd72a
ilbagatto <sergei.krushinski@gmail.com>
2025-05-03 18:41:50 +0300

    provider fixed

--- lib/providers/telegram_user_provider.dart
+++ lib/providers/telegram_user_provider.dart
@@ -1,26 +1,34 @@
+// lib/providers/telegram_user_provider.dart
+
 import 'package:flutter/material.dart';
 import 'package:tarot_mini_app/services/telegram_service.dart';
+import 'package:tarot_mini_app/services/logger_service.dart';
 
-import '../services/logger_service.dart';
-
-
+/// Provides Telegram user data to the widget tree
 class TelegramUserProvider extends ChangeNotifier {
+  final TelegramService _telegram = TelegramService();
+
   String? userId;
   String? username;
   String? firstName;
   String? lastName;
 
+  /// Returns true once userId is loaded
   bool get isLoaded => userId != null;
 
-  /// Инициализация пользователя
+  /// Loads the Telegram user data from WebApp initData
   Future<void> loadUser() async {
     try {
-      userId = TelegramWebAppService.getUserId();
-      username = TelegramWebAppService.getUsername();
-      firstName = TelegramWebAppService.getFirstName();
-      lastName = TelegramWebAppService.getLastName();
+      userId    = _telegram.userId;
+      username  = _telegram.username;
+      firstName = _telegram.firstName;
+      lastName  = _telegram.lastName;
+
       notifyListeners();
-      logger.i('Telegram user loaded: id=$userId, username=$username, firstName=$firstName');
+      logger.i(
+        'Telegram user loaded: '
+        'id=$userId, username=$username, firstName=$firstName, lastName=$lastName'
+      );
     } catch (e) {
       logger.e('Failed to load Telegram user: $e');
     }
